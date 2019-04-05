05657a4ae424f615aba2f762b1c8ad296a6a0d95
Austen Adler <agadler@austenadler.com>
2019-04-05 11:31:02 -0400

    Minor changes (allow notifications, SWs)

--- user.js
+++ user.js
@@ -15,11 +15,13 @@
 // NOTICE: Disabling ServiceWorkers breaks functionality on some sites (Google Street View...)
 // Unknown security implications
 // CVE-2016-5259, CVE-2016-2812, CVE-2016-1949, CVE-2016-5287 (fixed)
-user_pref("dom.serviceWorkers.enabled",				false);
+// NOTE: Disabled because this is now handled by uMatrix
+// user_pref("dom.serviceWorkers.enabled",				false);
 
 // PREF: Disable web notifications
 // https://support.mozilla.org/en-US/questions/1140439
-user_pref("dom.webnotifications.enabled",			false);
+// NOTE: Disabled because the notifications are not that annoying
+// user_pref("dom.webnotifications.enabled",			false);
 
 // PREF: Disable DOM timing API
 // https://wiki.mozilla.org/Security/Reviews/Firefox/NavigationTimingAPI
